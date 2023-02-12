34c9f4d8e7efcf5b0f1ec5cf8206e4a7ec1ad6d5
Moritz Gottschling <moritz@giantswarm.io>
2023-02-12 15:43:58 +0100

    Update to docker compose

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ set -e
 export WWWUSER=${UID}
 export WWWGROUP=${GID}
 
-./peasant/laravel/vendor/laravel/sail/bin/sail build && docker-compose up -d
+./peasant/laravel/vendor/laravel/sail/bin/sail build && docker compose up -d
 
-docker-compose exec api php artisan cache:clear
-docker-compose exec api php artisan config:cache
-docker-compose exec api php artisan route:cache
-docker-compose exec api php artisan view:cache
+docker compose exec api php artisan cache:clear
+docker compose exec api php artisan config:cache
+docker compose exec api php artisan route:cache
+docker compose exec api php artisan view:cache
 
-docker-compose exec api php artisan migrate
+docker compose exec api php artisan migrate
 
 

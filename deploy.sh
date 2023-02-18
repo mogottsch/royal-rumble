0d779c412333afedad960f086add909104837e18
Moritz Gottschling <moritz@giantswarm.io>
2023-02-18 19:28:58 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,19 +4,15 @@ set -e
 export WWWUSER=${UID:-1000}
 export WWWGROUP=${GID:-1000}
 
-cp .prod.env .env
-cp .env ./peasant/laravel/.env
-cp .env ./rumble/.env
+./compose.sh build && ./compose.sh up -d
 
-docker compose build && docker compose up -d
+./compose.sh exec api composer install
 
-docker compose exec api composer install
+./compose.sh exec api php artisan cache:clear
+./compose.sh exec api php artisan config:cache
+./compose.sh exec api php artisan route:cache
+./compose.sh exec api php artisan view:cache
 
-docker compose exec api php artisan cache:clear
-docker compose exec api php artisan config:cache
-docker compose exec api php artisan route:cache
-docker compose exec api php artisan view:cache
-
-docker compose exec api php artisan migrate
+./compose.sh exec api php artisan migrate
 
 

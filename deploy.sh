207c18134519ba2dc98509c061d904b83a9aa1e3
Moritz Gottschling <moritz@giantswarm.io>
2023-02-18 19:29:54 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@ set -e
 export WWWUSER=${UID:-1000}
 export WWWGROUP=${GID:-1000}
 
-./compose.sh build && ./compose.sh up -d
+./compose.sh prod build && ./compose.sh prod up -d
 
-./compose.sh exec api composer install
+./compose.sh prod exec api composer install
 
-./compose.sh exec api php artisan cache:clear
-./compose.sh exec api php artisan config:cache
-./compose.sh exec api php artisan route:cache
-./compose.sh exec api php artisan view:cache
+./compose.sh prod exec api php artisan cache:clear
+./compose.sh prod exec api php artisan config:cache
+./compose.sh prod exec api php artisan route:cache
+./compose.sh prod exec api php artisan view:cache
 
-./compose.sh exec api php artisan migrate
+./compose.sh prod exec api php artisan migrate
 
 

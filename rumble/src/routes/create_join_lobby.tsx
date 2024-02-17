476a5fd58e6d9998264bd48a8ec36a07b5cdd026
Moritz Gottschling <gottschling.moritz@gmail.com>
2024-02-17 10:00:11 +0100

    Add year and fireworks

--- rumble/src/routes/create_join_lobby.tsx
+++ rumble/src/routes/create_join_lobby.tsx
@@ -1,14 +1,26 @@
 import { Box } from "@mui/material";
 import { css } from "@emotion/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { InputField } from "../components/form";
 import { PrimaryButton } from "../components/buttons";
+import { Fireworks, type FireworksHandlers } from "@fireworks-js/react";
 
 export default function CreateJoinLobby() {
   const [lobbyCode, setLobbyCode] = useState("");
 
   const updateLobbyCode = (value: string) => {
     setLobbyCode(value.toUpperCase());
+  };
+
+  const ref = useRef<FireworksHandlers>(null);
+
+  const toggle = () => {
+    if (!ref.current) return;
+    if (ref.current.isRunning) {
+      ref.current.stop();
+    } else {
+      ref.current.start();
+    }
   };
 
   return (
@@ -20,6 +32,19 @@ export default function CreateJoinLobby() {
         justifyContent: "center",
       }}
     >
+      <h1 className="year">{new Date().getFullYear()}</h1>
+      <Fireworks
+        ref={ref}
+        options={{ mouse: { click: true, max: 5 }, intensity: 25 }}
+        style={{
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          position: "fixed",
+          // background: "#000",
+        }}
+      />
       <InputField
         label="Lobby code"
         htmlFor="lobby-code"

879999614ce620da1f2822cd2743ed1597273050
Moritz Gottschling <gottschling.moritz@gmail.com>
2025-02-23 14:56:48 +0100

    Update copy wrestler images script

--- scripts/copy-wrestler-images.sh
+++ scripts/copy-wrestler-images.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cp ./crawler/data/saved_superstars.json ./backend/laravel/storage/app/saved_superstars.json
+mkdir -p ./backend/laravel/storage/app/public/wrestlers
 cp ./crawler/data/images/* ./backend/laravel/storage/app/public/wrestlers/
